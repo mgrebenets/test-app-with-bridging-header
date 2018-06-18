371a0f8ae66a1a5b9397e796eee703c70b67d47c
Maksym Grebenets <maksym.grebenets@cba.com.au>
2018-06-18 22:24:50 +1000

    Change to code that fails

--- Sources/Bridging-Header.h
+++ Sources/Bridging-Header.h
@@ -1,4 +1,4 @@
 //  Copyright (c) 2018 Commonwealth Bank. All rights reserved.
 
-// #import "ObjCProtocol.h"
-#import "ObjC/ObjCProtocol.h"
+#import "ObjCProtocol.h"
+// #import "ObjC/ObjCProtocol.h"
